bc40b6506b33b66c7d957fd672ef919d368cf708
Zeke-Cachi <ezequielcachi@gmail.com>
2023-06-08 12:33:28 -0300

    removed more unnecessary imports

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const app = express();
 const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-const {connect, connectUser} = require('./db/db')
+const {connect} = require('./db/db')
 
 //CONTROLLERS
 const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
-const userRouter = require('./routes/users')
 
 
 
@@ -21,12 +20,12 @@ app.use(cors());
 
 app.use('/', indexRouter)
 app.use('/api/', apiRouter)
-app.use('/user/', userRouter)
+
 
 
 //CONNECT TO DB
 connect();
-connectUser();
+
 
 
 
