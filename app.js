bedaffa8022364b36ea1e91c653365a56a96c382
Zeke-Cachi <ezequielcachi@gmail.com>
2023-06-15 14:15:38 -0300

    changed cors access policy to allow deployed frontend site

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(logger('dev'));
 app.use(cors({ origin: '*' }));
 app.use((_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://music-store-stock-editor.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
